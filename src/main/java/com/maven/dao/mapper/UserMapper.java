7ced636c3d6455bc3f50fd5d62be009379159216
headean <386495531@qq.com>
2017-08-21 08:04:36 +0800

    ** 新增 dao 层接口 @Repository 注解，解决 service 层注入 dao 层对象报错问题 ** ** 修改 SpringMVC 配置文件，扫描注解范围改为所有包，注释不必要不明确代码块 ** 新增 log4j2 日志，暂不明确是否能使用 -- 昨晚一夜未眠，今天周一上班有点困啊，不说了push收拾上班。

--- src/main/java/com/maven/dao/mapper/UserMapper.java
+++ src/main/java/com/maven/dao/mapper/UserMapper.java
@@ -1,10 +1,12 @@
 package com.maven.dao.mapper;
 
 import com.maven.model.User;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 // mybatis 做接口映射
+@Repository
 public interface UserMapper {
     public List<User> findUserNameById(String user);
 }
