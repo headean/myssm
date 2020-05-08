19a623147f3809e212e612f376a8c824eff74fde
headean <386495531@qq.com>
2020-05-08 17:38:47 +0800

    block linkedlist

---
+++ src/main/java/com/maven/test/blocklinked/Message.java
@@ -0,0 +1,13 @@
+package com.maven.test.blocklinked;
+
+public class Message {
+	private int vac;
+
+	public int getVac() {
+		return vac;
+	}
+
+	public void setVac(int vac) {
+		this.vac = vac;
+	}
+}
